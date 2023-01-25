da77cfc519fcc4d1b53cdbc74959658d4c63b435
ibrahimsugun <1150606905@nku.edu.tr>
2023-01-25 04:20:58 +0300

    products and categories written , Category bar scheme written

---
+++ apps/site/src/lib/Components/product/productCategories.ts
@@ -0,0 +1,23 @@
+
+    export const categories = [
+         {   text: "Buhar Kazanları",
+            url: "#Kazans"},
+         {   text: "Paskaralar",
+            url: "#Paskaras"},
+         {   text: "Transfer Baskı Presleri",
+            url: "#TransferPresses"},
+         {   text:"Çakım Makinaları",
+            url: "#Nailings"},                  
+         {   text:"Leke Temizleme Makinaları",
+            url: "#StainCleanings"},           
+         {   text:"Silter Ütü Grubu",
+            url: "#SilterIroning"},           
+         {   text:"Mal Taşıma Arabaları",
+            url: "#TransportTrolleys"},           
+         {   text:"Kalite Kontrol Makinaları",
+            url: "QualityControl"},           
+         {   text:"Diğer Ürünler",
+            url: "#OtherProducts"},           
+         {   text:"Yedek Parça",
+            url: "#SparePart"}       
+        ]
---
+++ apps/site/src/lib/Components/product/productsWithCategories.ts
@@ -0,0 +1,373 @@
+export const products = [
+    {
+        category: 'Buhar Kazanları',
+        items: [
+        {
+            name: '4 Çıkışlı',
+            url: "/#",
+            description: "Buhar Kazanı",
+            explanation: ""
+        },
+        {
+            name: '3 Çıkışlı',
+            url: "/#",
+            description: "Buhar Kazanı",
+            explanation: ""
+        },
+        {
+            name: '2 Çıkışlı',
+            url: "/#",
+            description: "Buhar Kazanı",
+            explanation: ""
+        },
+        {
+            name: 'Doğalgazlı CE Belgeli İgdaş Onaylı',
+            url: "/#",
+            description: "Buhar Jeneratörü",
+            explanation: ""
+        },
+        {
+            name: '60KW',
+            url: "/#",
+            description: "Buhar Jeneratörü",
+            explanation: ""
+        },
+        {
+            name: '30KW',
+            url: "/#",
+            description: "Buhar Jeneratörü",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Paskaralar',
+        items: [
+        {
+            name: 'Işıklı Balanserli',
+            url: "/#",
+            description: "Geniş Isıtmasız Paskara",
+            explanation: ""
+        },
+        {
+            name: 'Kendinden Kazanlı',
+            url: "/#",
+            description: "Geniş Paskara",
+            explanation: ""
+        },
+        {
+            name: 'Isıtmasız',
+            url: "/#",
+            description: "Dar Paskara",
+            explanation: ""
+        },
+        {
+            name: 'Işıklı Balanserli Kendinden Kazanlı',
+            url: "/#",
+            description: "Geniş Paskara",
+            explanation: ""
+        },
+        {
+            name: 'Işıklı Balanserli',
+            url: "/#",
+            description: "Dar Kullu Paskara",
+            explanation: ""
+        },
+        {
+            name: 'Kendinden Kazanlı',
+            url: "/#",
+            description: "Dar Paskara",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Transfer Baskı Presleri',
+        items: [
+        {
+            name: '20-20 Gezer Kafa',
+            url: "/#",
+            description: "Transfer Baskı Presi",
+            explanation: ""
+        },
+        {
+            name: '20-20 Döner Kafa',
+            url: "/#",
+            description: "Transfer Geniş Paskara",
+            explanation: ""
+        },
+        {
+            name: '40-50 Taş Yapıştırma',
+            url: "/#",
+            description: "Pres Makinası",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Çakım Makinaları',
+        items: [
+        {
+            name: 'Motorlu',
+            url: "/#",
+            description: "Çakım Makinası",
+            explanation: ""
+        },
+        {
+            name: 'Motorlu',
+            url: "/#",
+            description: "Çakım Makinası",
+            explanation: ""
+        },
+        {
+            name: 'Elektrikli',
+            url: "/#",
+            description: "Çakım Makinası",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Leke Temizleme Makinaları',
+        items: [
+        {
+            name: 'Masa Tipi',
+            url: "/#",
+            description: "Leke Makinası",
+            explanation: ""
+        },
+        {
+            name: 'Dar Dipi Klasik',
+            url: "/#",
+            description: "Leke Makinası",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Silter Ütü Grubu',
+        items: [
+        {
+            name: 'Silter Süper Mini 1 Lt Manometreli',
+            url: "/#",
+            description: "Profesyonel Ütü",
+            explanation: ""
+        },
+        {
+            name: 'Magma 3,5 Lt',
+            url: "/#",
+            description: "Profesyonel Ütü",
+            explanation: ""
+        },
+        {
+            name: 'SPRMN 2035 Silter Süper Mini',
+            url: "/#",
+            description: "Profesyonel - 3,5 Lt",
+            explanation: ""
+        },
+        {
+            name: 'SPRMN 2005 Silter Süper Mini - 5 Lt',
+            url: "/#",
+            description: "Buharlı Ütü",
+            explanation: ""
+        },
+        {
+            name: 'SPRMN 2030 Gazella Silter Süper Mini',
+            url: "/#",
+            description: "Buhar Kazanlı Ütü",
+            explanation: ""
+        },
+        {
+            name: 'Potent Fan Dar Masa 3,5 Lt SMPSA 2135 APD',
+            url: "/#",
+            description: "Ütü Makinası",
+            explanation: ""
+        },
+        {
+            name: 'SPRMN 2036 3,5 Lt Askıda FD',
+            url: "/#",
+            description: "Buhar Kazanlı Ütü",
+            explanation: ""
+        },
+        {
+            name: 'Silter-Bitsy MiniSilter SM BM 2035 Çift Fırçalı',
+            url: "/#",
+            description: "Kazanlı Ütü",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Mal Taşıma Arabaları',
+        items: [
+        {
+            name: 'Çift Siperli',
+            url: "/#",
+            description: "Mal Taşıma Arabası",
+            explanation: ""
+        },
+        {
+            name: 'Ağır Hizmet Modeli',
+            url: "/#",
+            description: "Mal Taşıma Arabası",
+            explanation: ""
+        },
+        {
+            name: 'Plastik Tekne',
+            url: "/#",
+            description: "Mal Taşıma Arabası",
+            explanation: ""
+        },
+        {
+            name: 'Tekerlekli Hasır Tel Sepet',
+            url: "/#",
+            description: "Mal Taşıma Arabası",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Kalite Kontrol Masaları',
+        items: [
+        {
+            name: 'Üstten Işıklı Eğim Ayarlı',
+            url: "/#",
+            description: "Kalite Kontrol Masası",
+            explanation: ""
+        },
+        {
+            name: 'Sabit Eğimli',
+            url: "/#",
+            description: "Kalite Kontrol Masası",
+            explanation: ""
+        },
+        {
+            name: 'Sabit Eğimli Üstten Işıklı',
+            url: "/#",
+            description: "Kalite Kontrol Masası",
+            explanation: ""
+        },
+        {
+            name: 'Kanatlı',
+            url: "/#",
+            description: "Kalite Kontrol Masası",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Diğer Ürünler',
+        items: [
+        {
+            name: 'Yan Dikiş Açma',
+            url: "/#",
+            description: "Makinası",
+            explanation: ""
+        },
+        {
+            name: 'Pantolon Şişirme',
+            url: "/#",
+            description: "Robotu",
+            explanation: ""
+        },
+        {
+            name: 'Çift Kafa İplik Temizleme',
+            url: "/#",
+            description: "Robotu",
+            explanation: ""
+        },
+        {
+            name: 'Kılçık',
+            url: "/#",
+            description: "Makinası",
+            explanation: ""
+        }
+        ]
+    },
+    {
+        category: 'Yedek Parça',
+        items: [
+        {
+            name: 'Silter SY KF 3722Y Potent Salyangoz',
+            url: "/#",
+            description: "Radyal Fan",
+            explanation: ""
+        },
+        {
+            name: 'Buhar Kazanı Su Pompası',
+            url: "/#",
+            description: "",
+            explanation: ""
+        },
+        {
+            name: 'Silter STB 200G Buharlı El Ütüsü',
+            url: "/#",
+            description: "Sanayi Tipi Hafif Ütü",
+            explanation: ""
+        },
+        {
+            name: 'STB 295 Gazella "Slim" Dikiş Açma',
+            url: "/#",
+            description: "Buharlı El Ütüsü",
+            explanation: ""
+        },
+        {
+            name: 'Magma Buharlı',
+            url: "/#",
+            description: "El Ütüsü",
+            explanation: ""
+        },
+        {
+            name: 'Leke Tabancası',
+            url: "/#",
+            description: "",
+            explanation: ""
+        },
+        {
+            name: 'Kazancı Ütü Selenoid Valf',
+            url: "/#",
+            description: "",
+            explanation: ""
+        }
+        ]
+    }
+]
+
+/* 
+
+ //Creating DATABASE
+
+CREATE TABLE categories (
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE items (
+    id SERIAL PRIMARY KEY,
+    category_id INTEGER REFERENCES categories(id),
+    name VARCHAR(255) NOT NULL,
+    url VARCHAR(255) NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    explanation VARCHAR(255) NOT NULL
+); 
+
+*/
+
+
+/* 
+            //inserting Database 
+
+INSERT INTO categories (name) VALUES ('Buhar Kazanları');
+
+INSERT INTO items (category_id, name, url, description, explanation)
+VALUES 
+(1, '4 Çıkışlı', '/#', 'Buhar Kazanı', ''),
+(1, '3 Çıkışlı', '/#', 'Buhar Kazanı', ''),
+(1, '2 Çıkışlı', '/#', 'Buhar Kazanı', ''),
+(1, 'Doğalgazlı CE Belgeli İgdaş Onaylı', '/#', 'Buhar Jeneratörü', ''),
+(1, '60KW', '/#', 'Buhar Jeneratörü', ''),
+(1, '30KW', '/#', 'Buhar Jeneratörü', '');
+.... 
+
+
+*/
