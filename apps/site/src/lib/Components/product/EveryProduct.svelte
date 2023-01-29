cd483b30d5106205d438ae562bc14ab30f2b81b8
ibrahimsugun <1150606905@nku.edu.tr>
2023-01-30 01:20:40 +0300

    products and categories linked every header

---
+++ apps/site/src/lib/Components/product/EveryProduct.svelte
@@ -0,0 +1,34 @@
+<script context="module" lang="ts" >
+
+import { products } from "$lib/components/product/productsWithCategories";
+
+
+</script>
+
+
+    
+   
+
+    
+    {#each products as product}
+        <div class="col-12 mb-3">
+            <h3 class="font-weight-semi-bold ml-5" id="{product?.slug}">{product.category}</h3>
+        </div>
+            {#each product.items as item}
+            <div class="col-lg-4 col-md-6 col-sm-12 pb-1">
+                <div class="card product-item border-0 mb-4">
+                    <div class="card-header product-img position-relative overflow-hidden bg-transparent border p-0">
+                        <img class="img-fluid w-100" src="img/product-1.jpg" alt="Ürün Resmi">
+                    </div>
+                    <div class="card-body border-left border-right border text-center p-0 pt-4 pb-3">
+                        <h6 class="text-truncate mb-3">{item.name}</h6>
+                    </div>
+                    
+                    <div class="card-footer d-flex justify-content-between bg-light border">
+                        <a href="{item.url}" class="btn btn-sm text-dark p-0"><i class="fas fa-eye text-info mr-1"></i>Detayları Gör</a>
+                    </div>
+                </div>
+            </div>
+            {/each}
+    {/each}
+
---
+++ apps/site/src/lib/Components/product/ShopSidebar.svelte
@@ -0,0 +1,70 @@
+<script lang="ts">
+    import { products } from "$lib/components/product/productsWithCategories";
+</script>
+
+
+
+<div class="col-lg-3 col-md-12 position-static">
+    <!-- CategoriNav Start -->
+    <div class="border-bottom mb-4 pb-4 mt-5">
+        <h5 class="font-weight-semi-bold mb-4">Kategori Seçenekleri</h5>
+        <form>
+            {#each products.map(x => ({ text: x?.category,slug:x?.slug})) as product}
+                   <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <a class="custom-control-label" href="#{product.slug}">{product.text}</a>
+                 
+                </div>
+            {/each}
+         
+            <!-- <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Paskaralar">Paskaralar</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Transfer Baskı Presleri">Transfer Baskı Presleri</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <a class="custom-control-label" href="#cakim">Çakım Makinaları</a>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Leke Temizleme Makinaları">Leke Temizleme Makinaları</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Silter Ütü Grubu">Silter Ütü Grubu</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Mal Taşıma Arabaları">Mal Taşıma Arabaları</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Kalite Kontrol Masaları">Kalite Kontrol Masaları</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Diğer Ürünler">Diğer Ürünler</label>
+                 
+            </div>
+            <div class="custom-control custom-checkbox d-flex align-items-center justify-content-between mb-3">
+                 
+                <label class="custom-control-label" for="Yedek Parça">Yedek Parça</label>
+                 
+            </div> -->
+
+        </form>
+    </div>
+    <!-- CategoriNav End -->
+
+</div>
