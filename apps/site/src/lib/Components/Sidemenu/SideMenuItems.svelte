89bfaf7512c865e3c26e0b7f56236d373032b588
ibrahimsugun <1150606905@nku.edu.tr>
2023-02-05 15:38:32 +0300

    sidebar links re-writed and cleaned

--- apps/site/src/lib/Components/Sidemenu/SideMenuItems.svelte
+++ apps/site/src/lib/Components/Sidemenu/SideMenuItems.svelte
@@ -1,36 +1,6 @@
 <script>
    import { slide  } from 'svelte/transition'
-   import { products } from "$lib/components/product/productsWithCategories";
-   
-       /*  let sideMenuItems: {
-           text: string,                   //Bunu nasıl kullanacağız çözemedim. +page.svelte'nin içinde kalabalık 
-           url: string                     //olmasın diye items'i burada yazdım
-       }[] = [] */
-       
-         const items = [
-        {   text: "Buhar Kazanları",
-           url: "https://www.google.com"},
-        {   text: "Paskaralar",
-           url: "/"},
-        {   text: "Transfer Baskı Presleri",
-           url: "/"},
-        {   text: "Çakım Makinaları",
-           url: "/"},
-        {   text: "Leke Temizleme ",
-           url: "/"}    ,   
-        {   text: "Silter Ütü Grubu",
-           url: "/"}  ,      
-        {   text: "Diğer Ürünler",
-           url: "/"}   ,    
-        {   text: "Mal Taşıma Arabaları",
-           url: "/"}  ,     
-        {   text: "Kalite Kontrol Masaları",
-           url: "/"}  ,     
-        {   text: "Yedek Parça",
-           url: "/"}           
-       ]
-   
-    
+   import { products } from "$lib/components/product/productsWithCategories";   
    
    </script>
    
