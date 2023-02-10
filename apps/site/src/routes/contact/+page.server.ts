1f98f4c26556a3c33b0e76f55ccca0f89010af7a
ibrahimsugun <1150606905@nku.edu.tr>
2023-02-10 05:07:03 +0300

    inputs are checking in client side for now

--- apps/site/src/routes/contact/+page.server.ts
+++ apps/site/src/routes/contact/+page.server.ts
@@ -1,4 +1,4 @@
-import type { ServerLoad } from "@sveltejs/kit";
+import type { ServerLoad,Actions } from "@sveltejs/kit";
 
 export const load :ServerLoad = (() => {
 
@@ -6,3 +6,10 @@ export const load :ServerLoad = (() => {
         pageName: "İletişim"
     }
 }) as ServerLoad
+
+
+export const actions : Actions = {
+    default(){
+        console.log("ananze")
+    } 
+} 
